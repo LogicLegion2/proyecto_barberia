4c3c3c5ea6769db435a780414b8fdce93ce996e5
Paulina200R <paulinaramirezmorales12@gmail.com>
2024-06-05 21:04:02 -0500

    Corrección url de vistas

---
+++ frontend/src/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { config } from "dotenv";
+import ruta from "./routes/index.js";
+import ejs from "ejs";
+import path from "path";
+import morgan from "morgan";
+import cors from "cors";
+import { fileURLToPath } from 'url';
+config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = express();
+
+server.use(morgan("dev"));
+server.use(cors());
+server.set("view engine", "ejs");
+server.set("views", path.join(__dirname + "/views"))
+server.set("port", process.env.PORT || 3000);
+server.use(express.static(path.join(__dirname + "/public")));
+
+
+server.use("/", ruta);
+
+server.use("/", (req,res)=>{
+    res.render("archivoDeError")
+});
+
+export default server;
