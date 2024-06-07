e0a7fbccfaedd5f66b497955710fd51577f2f34f
keiner12345 <gzkeiner@gmail.com>
2024-06-06 20:15:39 -0500

    Ver productos y servicios modificado y bueno, ultimo

--- backend/app/controllers/servicios.controllers.js
+++ backend/app/controllers/servicios.controllers.js
@@ -1,19 +1,13 @@
 import { pool } from "../config/mysql.db.js";
 import { config } from "dotenv";
 import mysql from "mysql2/promise";
-
-// Configura las variables de entorno
 config();
 
 export const listarservicio = async (req, res) => {
     try {
-        // Ejecuta la consulta utilizando pool.execute
-        const [rows, fields] = await pool.execute("CALL LL_VER_SERVICIOS()");
-        
-        // Envía la respuesta JSON al cliente
-        res.json(rows);
+        const [respuesta] = await pool.query("CALL LL_VER_SERVICIOS()");
+        res.json(respuesta);
     } catch (error) {
-        // Maneja los errores
-        res.status(500).json({ error: error.message });
+        res.status(500).json(error);
     }
 };
