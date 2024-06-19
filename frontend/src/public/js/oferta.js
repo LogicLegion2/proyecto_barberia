014c28afd8ebe63369a894e6f1ad028c608ded47
Paulina200R <paulinaramirezmorales12@gmail.com>
2024-06-18 22:56:37 -0500

    Metodo buscar listo en página principal

--- frontend/src/public/js/oferta.js
+++ frontend/src/public/js/oferta.js
@@ -9,3 +9,52 @@ function redireccionarEditar() {
         alert('Seleccione una oferta primero');
     }
 }
+
+async function eliminarOferta() {
+    const id = localStorage.getItem('ofertaSeleccionada');
+    if (id) {
+        const respuesta = await fetch('http://localhost:3000/ofertas/desactivar', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ id: id })
+        });
+
+        if (respuesta.ok) {
+            Swal.fire({
+                icon: 'success',
+                title: "<h5 style='color:white; font-family: 'Aleo', serif;'>" + 'Oferta eliminada exitosamente' + "</h5>",
+                showConfirmButton: false,
+                timer: 1500,
+                customClass: {
+                    popup: 'bg-alert',
+                    content: 'text-alert'
+                }
+            });
+            setTimeout(() => {
+                window.location.reload();
+            }, 1500);
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: "<h5 style='color:white; font-family: 'Aleo', serif;'>" + 'Intentalo de nuevo más tarde' + "</h5>",
+                showConfirmButton: false,
+                timer: 1500,
+                customClass: {
+                    popup: 'bg-alert',
+                }
+            });
+        }
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: "<h5 style='color:white; font-family: 'Aleo', serif;'>" + 'Seleccione una oferta primero' + "</h5>",
+            showConfirmButton: false,
+            timer: 1500,
+            customClass: {
+                popup: 'bg-alert',
+            }
+        });
+    }
+}
--- frontend/src/public/js/ubicacion.js
+++ frontend/src/public/js/ubicacion.js
@@ -9,3 +9,52 @@ function redireccionarEditar() {
         alert('Seleccione una ubicación primero');
     }
 }
+
+async function eliminarUbicacion() {
+    const id = localStorage.getItem('ubicacionSeleccionada');
+    if (id) {
+        const respuesta = await fetch('http://localhost:3000/ubicaciones/desactivar', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ id: id })
+        });
+
+        if (respuesta.ok) {
+            Swal.fire({
+                icon: 'success',
+                title: "<h5 style='color:white; font-family: 'Aleo', serif;'>" + 'Ubicación eliminada exitosamente' + "</h5>",
+                showConfirmButton: false,
+                timer: 1500,
+                customClass: {
+                    popup: 'bg-alert',
+                    content: 'text-alert'
+                }
+            });
+            setTimeout(() => {
+                window.location.reload();
+            }, 1500);
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: "<h5 style='color:white; font-family: 'Aleo', serif;'>" + 'Intentalo de nuevo más tarde' + "</h5>",
+                showConfirmButton: false,
+                timer: 1500,
+                customClass: {
+                    popup: 'bg-alert',
+                }
+            });
+        }
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: "<h5 style='color:white; font-family: 'Aleo', serif;'>" + 'Seleccione una ubicación primero' + "</h5>",
+            showConfirmButton: false,
+            timer: 1500,
+            customClass: {
+                popup: 'bg-alert',
+            }
+        });
+    }
+}
