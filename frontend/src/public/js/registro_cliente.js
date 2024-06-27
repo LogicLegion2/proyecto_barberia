10991cfe4ed9ae7d7cadbcf2aeca4767a2971b7e
keiner12345 <gzkeiner@gmail.com>
2024-06-26 23:08:15 -0500

    Registro listo

---
+++ frontend/src/public/js/registro_cliente.js
@@ -0,0 +1,59 @@
+document.getElementById("registrarCliente").addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const telefono = document.getElementById("telefono").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const datosUsuario = {
+        nombre: name, // Asegúrate que coincida con el nombre esperado en tu backend (nombre en lugar de name)
+        telefono: telefono,
+        email: email,
+        contrasena: password, // Asegúrate que coincida con el nombre esperado en tu backend (contrasena en lugar de password)
+    };
+
+    fetch(`http://localhost:3000/usuarios/registro`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datosUsuario)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log("Usuario agregado:", data);
+        // Aquí podrías mostrar una alerta o realizar alguna acción adicional después de agregar el usuario
+        Swal.fire({
+            icon: 'success',
+            title: "<h5 style='color:white; font-family: \"Aleo\", serif;'>Usuario registrado exitosamente</h5>",
+            showConfirmButton: false,
+            timer: 1500,
+            customClass: {
+                popup: 'bg-alert',
+                content: 'text-alert'
+            }
+        });
+        // Opcional: Redireccionar después de un tiempo
+        setTimeout(() => {
+            window.location.href = '/ruta-donde-quieres-redireccionar'; // Cambia por la ruta deseada
+        }, 1500);
+    })
+    .catch(error => {
+        console.error("Fetch error:", error);
+        Swal.fire({
+            icon: 'error',
+            title: "<h5 style='color:white; font-family: \"Aleo\", serif;'>Error al registrar usuario</h5>",
+            showConfirmButton: false,
+            timer: 1500,
+            customClass: {
+                popup: 'bg-alert',
+            }
+        });
+    });
+});
